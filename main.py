ccb0f8c3b55ab1fe1ea7bb75203d8b05f3f5cb83
RSMagneto <zhangkainuc@163.com>
2022-06-28 01:25:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -57,22 +57,22 @@ if __name__ == '__main__':
     gaussian_conv_4 = functions.GaussianBlurConv(opt.channels - 1).to(opt.device)
     gaussian_conv_1 = functions.GaussianBlurConv(1).to(opt.device)
 
-    # LF-Net
-    poolingnet = model.poolingNet(opt.channels).to(opt.device)
-    for module in poolingnet.modules():
+    net = model.poolingNet(opt.channels).to(opt.device)
+#     net = model.UNet(opt.channels, opt.channels-1).to(opt.device)
+    for module in net.modules():
         if isinstance(module, nn.BatchNorm2d):
             module.eval()
 
-    optimizer1 = torch.optim.Adam(poolingnet.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
+    optimizer1 = torch.optim.Adam(net.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
     scheduler1 = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizer1,milestones=[1600],gamma=opt.gamma)
 
     loss = torch.nn.L1Loss()
-    best_weights = copy.deepcopy(poolingnet.state_dict())
+    best_weights = copy.deepcopy(net.state_dict())
     best_epoch = 0
     best_SAM=1.0
 
     for i in range(opt.epoch):
-        poolingnet.train()
+        net.train()
         epoch_losses = functions.AverageMeter()
         batch_time = functions.AverageMeter()
         end = time()
@@ -83,29 +83,32 @@ if __name__ == '__main__':
                 panBatch = Variable(panBatch.to(torch.float32))
                 gtBatch = Variable(gtBatch.to(torch.float32))
             N = len(train_loader)
-            poolingnet.zero_grad()
+            net.zero_grad()
             msBatch = torch.nn.functional.interpolate(msBatch, size=(gtBatch.shape[2], gtBatch.shape[3]),
                                                       mode='bilinear')
 
-            lowpass=poolingnet(gaussian_conv_1(panBatch),gaussian_conv_4(msBatch))
+            lowpass=net(gaussian_conv_1(panBatch),gaussian_conv_4(msBatch))
             lowLoss = loss(lowpass,gaussian_conv_4(gtBatch))
             lowLoss.backward()
+#             highpass = net(panBatch - gaussian_conv_1(panBatch), msBatch - gaussian_conv_4(msBatch))
+#             highLoss=loss(highpass,gtBatch - gaussian_conv_4(gtBatch))
+#             highLoss.backward()
             optimizer1.step()
-            epoch_losses.update(lowLoss.item(), msBatch.shape[0])
+            epoch_losses.update(lowLoss.item(), msBatch.shape[0])  # highLoss
             batch_time.update(time() - end)
             end = time()
             if (batch_idx + 1) % 100 == 0:
                 training_state = '  '.join(
-                    ['Epoch: {}', '[{} / {}]', 'mseLoss: {:.6f}']
+                    ['Epoch: {}', '[{} / {}]', 'Loss: {:.6f}']
                 )
                 training_state = training_state.format(
                     i, batch_idx, N, lowLoss
-                )
+                )   # highLoss
                 print(training_state)
         print('%d epoch: loss is %.6f, epoch time is %.4f' % (i, epoch_losses.avg, batch_time.avg))
-        torch.save(poolingnet.state_dict(), os.path.join(opt.outputs_dir, 'lowpass/epoch_{}.pth'.format(i)))
+        torch.save(net.state_dict(), os.path.join(opt.outputs_dir, 'lowpass/epoch_{}.pth'.format(i)))   # highLoss
 
-        poolingnet.eval()
+        net.eval()
         epoch_SAM=functions.AverageMeter()
         with torch.no_grad():
             for j, (gtTest, msTest, panTest) in enumerate(val_loader):
@@ -115,87 +118,18 @@ if __name__ == '__main__':
                     panTest = Variable(panTest.to(torch.float32))
                     gtTest = Variable(gtTest.to(torch.float32))
                 msTest = torch.nn.functional.interpolate(msTest, size=(256, 256), mode='bilinear')
-                lowpass=poolingnet(gaussian_conv_1(panTest),gaussian_conv_4(msTest))
+                lowpass=net(gaussian_conv_1(panTest),gaussian_conv_4(msTest))
                 test_SAM=functions.SAM(lowpass, gaussian_conv_4(gtTest))
+#                 highpass = net(panTest - gaussian_conv_1(panTest), msTest - gaussian_conv_4(msTest))
+#                 test_SAM = functions.SAM(highpass, gtTest - gaussian_conv_4(gtTest))
                 if test_SAM==test_SAM:
                     epoch_SAM.update(test_SAM,lowpass.shape[0])
-            print('poolingnet eval SAM: {:.6f}'.format(epoch_SAM.avg))
+            print('net eval SAM: {:.6f}'.format(epoch_SAM.avg))
         if epoch_SAM.avg < best_SAM:
             best_epoch = i
             best_SAM = epoch_SAM.avg
-            best_weights = copy.deepcopy(poolingnet.state_dict())
+            best_weights = copy.deepcopy(net.state_dict())
         print('best epoch:{:.0f}'.format(best_epoch))
         scheduler1.step()
-    print('poolingnet best epoch: {}, epoch_SAM: {:.6f}'.format(best_epoch, best_SAM))
-    torch.save(best_weights, os.path.join(opt.outputs_dir, 'lowpass/best.pth'))
-
-    # HF-Net
-    unet = model.UNet(opt.channels,opt.channels-1).to(opt.device)
-    for module in unet.modules():
-        if isinstance(module, nn.BatchNorm2d):
-            module.eval()
-
-    optimizer2 = torch.optim.Adam(unet.parameters(), lr=opt.lr, betas=(opt.beta1, 0.999))
-    scheduler2 = torch.optim.lr_scheduler.MultiStepLR(optimizer=optimizer2,milestones=[1600],gamma=opt.gamma)
-
-    loss = torch.nn.L1Loss()
-    best_weights = copy.deepcopy(unet.state_dict())
-    best_epoch = 0
-    best_SAM=1.0
-
-    for i in range(opt.epoch):
-        unet.train()
-        epoch_losses = functions.AverageMeter()
-        batch_time = functions.AverageMeter()
-        end = time()
-        for batch_idx, (gtBatch, msBatch, panBatch) in enumerate(train_loader):
-            if torch.cuda.is_available():
-                msBatch, panBatch, gtBatch = msBatch.cuda(), panBatch.cuda(), gtBatch.cuda()
-                msBatch = Variable(msBatch.to(torch.float32))
-                panBatch = Variable(panBatch.to(torch.float32))
-                gtBatch = Variable(gtBatch.to(torch.float32))
-            N = len(train_loader)
-            unet.zero_grad()
-            msBatch = torch.nn.functional.interpolate(msBatch, size=(gtBatch.shape[2], gtBatch.shape[3]),
-                                                      mode='bilinear')
-
-            highpass = unet(panBatch - gaussian_conv_1(panBatch), msBatch - gaussian_conv_4(msBatch))
-            lowLoss=loss(highpass,gtBatch - gaussian_conv_4(gtBatch))
-            lowLoss.backward()
-            optimizer2.step()
-            epoch_losses.update(lowLoss.item(), msBatch.shape[0])
-            batch_time.update(time() - end)
-            end = time()
-            if (batch_idx + 1) % 100 == 0:
-                training_state = '  '.join(
-                    ['Epoch: {}', '[{} / {}]', 'mseLoss: {:.6f}']
-                )
-                training_state = training_state.format(
-                    i, batch_idx, N, lowLoss
-                )
-                print(training_state)
-        print('%d epoch: loss is %.6f, epoch time is %.4f' % (i, epoch_losses.avg, batch_time.avg))
-        torch.save(unet.state_dict(), os.path.join(opt.outputs_dir, 'highpass/epoch_{}.pth'.format(i)))
-        unet.eval()
-        epoch_SAM=functions.AverageMeter()
-        with torch.no_grad():
-            for j, (gtTest, msTest, panTest) in enumerate(val_loader):
-                if torch.cuda.is_available():
-                    msTest, panTest, gtTest = msTest.cuda(), panTest.cuda(), gtTest.cuda()
-                    msTest = Variable(msTest.to(torch.float32))
-                    panTest = Variable(panTest.to(torch.float32))
-                    gtTest = Variable(gtTest.to(torch.float32))
-                msTest = torch.nn.functional.interpolate(msTest, size=(256, 256), mode='bilinear')
-                highpass = unet(panTest - gaussian_conv_1(panTest), msTest - gaussian_conv_4(msTest))
-                test_SAM = functions.SAM(highpass, gtTest - gaussian_conv_4(gtTest))
-                if test_SAM==test_SAM:
-                    epoch_SAM.update(test_SAM,highpass.shape[0])
-            print('unet eval SAM: {:.6f}'.format(epoch_SAM.avg))
-        if epoch_SAM.avg < best_SAM:
-            best_epoch = i
-            best_SAM = epoch_SAM.avg
-            best_weights = copy.deepcopy(unet.state_dict())
-        print('best epoch:{:.0f}'.format(best_epoch))
-        scheduler2.step()
-    print('unet best epoch: {}, epoch_SAM: {:.6f}'.format(best_epoch, best_SAM))
-    torch.save(best_weights, os.path.join(opt.outputs_dir, 'highpas/best.pth'))
+    print('net best epoch: {}, epoch_SAM: {:.6f}'.format(best_epoch, best_SAM))
+    torch.save(best_weights, os.path.join(opt.outputs_dir, 'lowpass/best.pth'))   # highLoss
